0a0364f320cd5ab869b2db17d57c55eaab68ac81
Ruy Outor <ruy.outor@gmail.com>
2022-11-12 01:01:58 -0300

    Lecture 32

---
+++ order-service/order-application/src/main/java/com/food/ordering/system/order/service/application/exception/handler/OrderGlobalExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.food.ordering.system.order.service.application.exception.handler;
+
+import com.food.ordering.system.application.handler.ErrorDTO;
+import com.food.ordering.system.application.handler.GlobalExceptionHandler;
+import com.food.ordering.system.order.service.domain.exception.OrderDomainException;
+import com.food.ordering.system.order.service.domain.exception.OrderNotFoudException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Slf4j
+@ControllerAdvice
+public class OrderGlobalExceptionHandler extends GlobalExceptionHandler {
+
+    @ResponseBody
+    @ExceptionHandler(value = {OrderDomainException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDTO handlerException(OrderDomainException orderDomainException){
+        log.error(orderDomainException.getMessage(), orderDomainException);
+        return ErrorDTO.builder()
+                .code(HttpStatus.BAD_REQUEST.getReasonPhrase())
+                .message(orderDomainException.getMessage())
+                .build();
+    }
+
+    @ResponseBody
+    @ExceptionHandler(value = {OrderDomainException.class})
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorDTO handlerException(OrderNotFoudException orderNotFoudException){
+        log.error(orderNotFoudException.getMessage(), orderNotFoudException);
+        return ErrorDTO.builder()
+                .code(HttpStatus.NOT_FOUND.getReasonPhrase())
+                .message(orderNotFoudException.getMessage())
+                .build();
+    }
+}
