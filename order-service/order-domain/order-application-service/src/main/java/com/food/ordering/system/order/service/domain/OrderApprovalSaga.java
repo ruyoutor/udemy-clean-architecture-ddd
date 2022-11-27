4c23a6f921072fc4845c676078c71716cd682400
Ruy Outor <ruy.outor@gmail.com>
2022-11-27 15:10:34 -0300

    Lecture 63

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderApprovalSaga.java
@@ -0,0 +1,54 @@
+package com.food.ordering.system.order.service.domain;
+
+import com.food.ordering.system.domain.event.EmptyEvent;
+import com.food.ordering.system.order.service.domain.dto.message.RestaurantApprovalResponse;
+import com.food.ordering.system.order.service.domain.entity.Order;
+import com.food.ordering.system.order.service.domain.event.OrderCancelledEvent;
+import com.food.ordering.system.order.service.domain.ports.output.message.publish.payment.OrderCancelledPaymentRequestMessagePublisher;
+import com.food.ordering.system.saga.SagaStep;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Component
+public class OrderApprovalSaga implements SagaStep<RestaurantApprovalResponse, EmptyEvent, OrderCancelledEvent> {
+
+    private final OrderDomainService orderDomainService;
+    private final OrderSagaHelper orderSagaHelper;
+    private final OrderCancelledPaymentRequestMessagePublisher orderCancelledPaymentRequestMessagePublisher;
+
+    public OrderApprovalSaga(OrderDomainService orderDomainService,
+                             OrderSagaHelper orderSagaHelper,
+                             OrderCancelledPaymentRequestMessagePublisher
+                                     orderCancelledPaymentRequestMessagePublisher) {
+        this.orderDomainService = orderDomainService;
+        this.orderSagaHelper = orderSagaHelper;
+        this.orderCancelledPaymentRequestMessagePublisher = orderCancelledPaymentRequestMessagePublisher;
+    }
+
+    @Override
+    @Transactional
+    public EmptyEvent process(RestaurantApprovalResponse approvalResponse) {
+        log.info("Approving order with id: {}", approvalResponse.getOrderId());
+        Order order = orderSagaHelper.findOrder(approvalResponse.getOrderId());
+        orderDomainService.approveOrder(order);
+        orderSagaHelper.saveOrder(order);
+        log.info("Order with id: {} is approved", order.getId().getValue());
+        return EmptyEvent.INSTANCE;
+    }
+
+    @Override
+    @Transactional
+    public OrderCancelledEvent rollback(RestaurantApprovalResponse approvalResponse) {
+        log.info("Cancelling order with id: {}", approvalResponse.getOrderId());
+        Order order = orderSagaHelper.findOrder(approvalResponse.getOrderId());
+        OrderCancelledEvent orderCancelledEvent = orderDomainService.cancelOrderPayment(
+                order,
+                approvalResponse.getFailureMessages(),
+                orderCancelledPaymentRequestMessagePublisher);
+        orderSagaHelper.saveOrder(order);
+        log.info("Order with id: {} is cancelling", order.getId().getValue());
+        return orderCancelledEvent;
+    }
+}
